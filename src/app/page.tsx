b4f766e63da8983e538804852bd5d3fa731638a8
mws75 <mwspencer75@gmail.com>
2025-08-22 10:07:44 -0700

    sign in page created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,24 @@
 import Image from "next/image";
 import {useState, useEffect} from "react" 
 import Dashboard from "@/app/components/dashboard";
+import {useUser} from "@clerk/nextjs";
+import {redirect} from "next/navigation";
+
+
+
+
 export default function Home(){
   const [items, setItems] = useState([]);
   const [search, setSearch] = useState("");
+  const {isLoaded, isSignedIn, user} = useUser();
+  
+  if(!isLoaded){
+    return null;
+  }
 
+  if(!isSignedIn){
+    redirect("/signin");
+  }
   
   // Button Searchh 
 
@@ -14,9 +28,8 @@ export default function Home(){
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <div className="p-8 bg-white rounded-lg shadow-md max-w-2xl text-center">
-        <h1 className="text-3xl font-bold mb-4">Welcome to Find My All</h1>
+        <h1 className="text-3xl font-bold mb-4">Welcome to Find My All {user?.firstName}</h1>
           <Dashboard/>
-
       </div>
 
     </div>
--- todo.md
+++ todo.md
@@ -2,4 +2,7 @@
 
 ## 1. Enable Clerk
     [x] Set Up Clerk Login
+    
+## feat_create_user_at_login
+    [x] Create Login Screen - https://clerk.com/docs/references/nextjs/custom-sign-in-or-up-page 
     [] When User Logs in, if they have not logged in before it will create a new user. 
